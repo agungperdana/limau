355acf3f23999d062bf29d9f929f94f2fd644497
Agung Dodi Perdana <agung.dodi@unit4.com>
2024-10-29 16:45:50 +0700

    add initial project

---
+++ src/test/java/com/mentesolution/limau/LimauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentesolution.limau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
